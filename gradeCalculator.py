694c15674316155d77819f523458e07d105d8f5e
Joseph Shields <Joseph.Shields@vu.com>
2025-01-26 03:28:33 -0600

    third attempt finally

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -2,12 +2,13 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
-    Bad_Score_message = "Bad Score"
+    bad_score_message = "Bad score"
     try:
-        score = float(input("Enter Score: "))
+        score = float(input("Enter score: "))
+        
 
         if score > 1.0:
-            grade = "Bad Score"
+            grade = bad_score_message
         elif score>= 0.9 and score <= 1.0:
             grade = "A"
         elif score >= 0.8:
@@ -16,17 +17,15 @@ def calculateGrade():
             grade = "C"
         elif score >= 0.6:
             grade = "D"
-        elif score < 0.6:
+        elif score < 0.6 and score >= 0:
             grade = "F"
         else:
-            grade = "Bad Score"
+            grade = bad_score_message
     
     
         
-         
-
     except:
-        grade = "Bad Score"
+        grade = bad_score_message
 
     print(grade)
 
